d47f870cc07d77fc4d898ac9282a60b11e06766d
superheroes6 <santibox888@gmail.com>
2026-01-05 14:35:35 +0100

    commit 2

--- practicas_so/6-switch.c
+++ practicas_so/6-switch.c
@@ -3,38 +3,52 @@
 int main(){
     char calculadora;
     double num1, num2;
+    int variable = 1;
 
-    printf("\nEscribe un operador: ");
-    scanf("%c", &calculadora);
+    while(variable){
+        printf("\nEscribe un operador o X para parar: ");
+        scanf("%c", &calculadora);
 
-    printf("\n Escribe el primer numero para la operacion: ");
-    scanf("%lf", &num1);
+        if (calculadora == 'X' || calculadora == 'x') {
+            printf("El programa ha finalizado.\n");
+            variable = 0;
+            break;   
+        }
 
-    printf("\n Escribe el segundo numero para la operacion: ");
-    scanf("%lf", &num2);
+        printf("\nEscribe el primer numero para la operacion: ");
+        scanf("%lf", &num1);
 
-    switch(calculadora){
-        case '+':
-        double suma = num1 + num2;
-        printf("\nResultado: %lf\n", suma);
-        break;
+        printf("\nEscribe el segundo numero para la operacion: ");
+        scanf("%lf", &num2);
 
-        case '-':
-        double resta = num1 - num2;
-        printf("\nResultado: %lf\n", resta);
-        break;
+        switch(calculadora){
+            case '+':
+            double suma = num1 + num2;
+            printf("\nResultado: %lf\n", suma);
+            break;
 
-        case '*':
-        double multiplicacion = num1 * num2;
-        printf("\nResultado: %lf\n", multiplicacion);
-        break;
+            case '-':
+            double resta = num1 - num2;
+            printf("\nResultado: %lf\n", resta);
+            break;
 
-        case '/':
-        double division = num1 / num2;
-        printf("\nResultado: %lf\n", division);
-        break;
+            case '*':
+            double multiplicacion = num1 * num2;
+            printf("\nResultado: %lf\n", multiplicacion);
+            break;
 
+            case '/':
+            if(num2 == 0){
+                    printf("Error: No se puede dividir por cero.\n");
+                } else {
+                    printf("Resultado: %lf\n", num1 / num2);
+                }
+                break;
+
+            default:
+                printf("Operador no valido. Intentalo de nuevo.\n");
+
+        }
     }
-
     return 0;
 }
